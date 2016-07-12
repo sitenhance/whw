60ca260d34d10767e678e020532f96e5817b3abf
alpham79 <alpham79@gmail.com>
2016-07-12 15:16:29 -0500

    Changed Press Page - Press Images to have repeater
    Added the repeater to the press page

--- js/controller/pressCtrl.js
+++ js/controller/pressCtrl.js
@@ -3,20 +3,10 @@
         pagesService.pressPage.get(function (res) {
             console.log(res);
             $scope.press_links = res.acf.press_links;
-            $scope.pressImage1 = res.acf.press_image_1.url;
-            $scope.pressImage1url = res.acf.press_image_1_url;
-            $scope.pressImage2 = res.acf.press_image_2.url;
-            $scope.pressImage2url = res.acf.press_image_2_url;
+            $scope.pressImages = res.acf.press_images;
             $scope.pressRelease = res.acf.press_release;
             $scope.media_kit = res.acf.media_kit;
             $scope.social = res.acf.social;
-
-//            $scope.featuredImages = _.chain(res.acf.featured_images[0])
-//                .filter(function (item) {
-//                        return item;
-//                    })
-//                    .value();
-            
             $scope.featuredImages = res.acf.featured_images;
             $scope.downloadImages = res.acf.download_images;
             
