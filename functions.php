25bf0ae103b493f5fde1b1496cf2310b19696d6f
Halbert Rivera <Halbert Rivera>
2016-07-25 13:59:47 -0500

    Added Angular Social Directives

--- functions.php
+++ functions.php
@@ -73,7 +73,8 @@
             wp_enqueue_script('resultsService', get_template_directory_uri() . '/js/services/results-service.js', array('ngScripts'), false);
             wp_enqueue_script('carouselItemDirective', get_template_directory_uri() . '/js/directives/carousel-item-directive.js', array('ngScripts'), false);
             wp_enqueue_script('searchDirective', get_template_directory_uri() . '/js/directives/search-directive.js', array('ngScripts'), false);
-            wp_enqueue_script('answerButtonDirective', get_template_directory_uri() . '/js/directives/answer-button-directive.js', array('ngScripts'), false);            
+            wp_enqueue_script('answerButtonDirective', get_template_directory_uri() . '/js/directives/answer-button-directive.js', array('ngScripts'), false);
+            wp_enqueue_script('socialIconsDirective', get_template_directory_uri() . '/js/directives/social-icons-directive.js', array('ngScripts'), false);            
             wp_enqueue_script('readMoreDirective', get_template_directory_uri() . '/js/directives/read-more-directive.js', array('ngScripts'), false);
         }
         function modify_jquery_version() {
